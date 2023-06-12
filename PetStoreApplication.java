e4afde5ac3c2b034486715559ec8872e740933e5
Jami <jamitvelez@gmail.com>
2023-06-12 14:53:52 -0700

    Week 13 Pet Store
    Pet store 1st commit

---
+++ PetStoreApplication.java
@@ -0,0 +1,16 @@
+package pet.stores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class PetStoreApplication {
+
+
+  public static void main(String[] args) {
+   SpringApplication.run(PetStoreApplication.class, args);
+
+  }
+
+}
